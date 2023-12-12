29c17a1d9eac95934b1a44fd8960383d360f2c77
Dejan Senborn <dejan.senborn@crealogix.com>
2023-12-12 19:42:39 +0100

    Chapter05 finished

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -3,17 +3,24 @@ import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Instant;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
 	@Test
 	void whenPostRequestThenBookCreated() {
-		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90, "Polarsophia");
+
 		webTestClient
 				.post()
 				.uri("/books")
@@ -22,8 +29,7 @@ class CatalogServiceApplicationTests {
 				.expectStatus().isCreated()
 				.expectBody(Book.class).value(actualBook -> {
 					assertThat(actualBook).isNotNull();
-					assertThat(actualBook.isbn())
-							.isEqualTo(expectedBook.isbn());
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
 				});
 	}
 }
